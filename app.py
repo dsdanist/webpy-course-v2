f3f5c9852b2e07f07054b15565796d40f927bef1
dsdanist <43006800+dsdanist@users.noreply.github.com>
2023-05-08 20:33:10 +0000

    some changes and jobitem

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Barcelona',
+  'salary': '100.000€'
+}, {
+  'id': 2,
+  'title': 'Data Scientist',
+  'location': 'Valencia',
+  'salary': '120.000€'
+}, {
+  'id': 3,
+  'title': 'Data Lead',
+  'location': 'Alicante',
+  'salary': '150.000€'
+}]
+
+
 @app.route("/")
 def hello_world():
-  return "Hello, world"
+  return render_template('home.html', jobs=JOBS, company_name='Aden')
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
