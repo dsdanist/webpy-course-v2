a8f2233059b6b8fa30e510dab817ffd10a5aa9c4
dsdanist <43006800+dsdanist@users.noreply.github.com>
2023-05-28 12:06:30 +0000

    database

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine, text
 import os
 
-db_connection_string = os.environ['DB_CONNECTION_STRING']
+#db_connection_string = os.environ['DB_CONNECTION_STRING']
+db_connection_string = "mysql+pymysql://nwmmxk7iaour2650idac:pscale_pw_gIpW8uBNJGQnklspWNjlYSNIumHjcSV7jvBCwxmd7s8@aws.connect.psdb.cloud/adecareers?charset=utf8mb4"
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
@@ -13,6 +14,7 @@ def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
     jobs = []
-    for row in result.all():
-      jobs.append(dict(row))
+    result_all = result.all()
+    for row in result_all:
+      jobs.append(row)
     return jobs
